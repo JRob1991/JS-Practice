1e2ef875c8213a0ab7f9d8e036f840de1024ba6c
JRob1991 <jprobinson9791@gmail.com>
2015-09-28 13:20:23 -0400

    Added ReadMe and finished .js

---
+++ main.js
@@ -0,0 +1,80 @@
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+// Q: What does `givenName` equal right now?
+// A: undefined
+
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// A: "Tim"
+
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+// A: "JD"
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// A: undefined
+
+
+/*=========================================================
+     Part 2: Simple Math
+*/
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: undefined (40 if you just use "math = high-low;")
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: 45
+
+
+/* =========================================================
+     Part 3: Expressions
+*/
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 32 years old"
+// The answer shoud not be written in a comment.
+
+var born = 1983;
+var today = 2015;
+
+// Answers Below:
+
+var born = 1983;
+var today  = 2015;
+var age  = today - born;
+if (age === 32)
+    console.log ("Tim is 32 years old")
+else
+    console.log ("Tim is old!")
+
+
+// Store some information following in variables.
+
+var yourName = "Justin";
+var instructorName = "Tim & JD";
+
+// Update the variables above so the expression reads correctly.
+// Answers Below:
+
+yourName = "Justin";
+instructorName = "Tim & JD"
+
+// Final Statement
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+
+
+
+
